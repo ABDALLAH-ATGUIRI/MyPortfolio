b800f5a1998957253bd3e88e0390a7a5a9bb3d3d
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2022-12-29 02:28:30 +0100

    add sevirces and skills

--- frontend/src/Pages/Resume/Resume.jsx
+++ frontend/src/Pages/Resume/Resume.jsx
@@ -1,12 +1,16 @@
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Icon, Paper, Typography } from '@material-ui/core'
 import React from 'react'
+import CustomTimeline, { CustomTimelineSeparator } from '../../Components/TimeLine/Timeline'
 import resumeData from '../../utils/resumeData'
+import WorkIcon from "@material-ui/icons/Work"
+import SchoolIcon from "@material-ui/icons/School"
 import "./Resume.css"
+import { TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@material-ui/lab'
 function Resume() {
   return (
     <>
       {/* About me */}
-      <Grid container className='section pb_30'>
+      <Grid container className='section pb_30 pt_45'>
         <Grid item className='section_title mb_30'>
           <span></span>
           <h6 className='section_title_text'>About Me</h6>
@@ -17,18 +21,99 @@ function Resume() {
       </Grid>
 
       {/* Education and experiences */}
-      <Grid container className='section'>
+      <Grid container className='section  pb_45'>
+        <Grid item className='section_title mb_30'>
+          <span></span>
+          <h6 className='section_title_text'>Resume</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container className='resume_timeline'>
+            {/* Experiences */}
+            <Grid item sm={12} md={6} >
+              <CustomTimeline title={"Work Experience"} icon={<WorkIcon />} children={""}  >
+                {resumeData.experiences.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className='timeline_title' >{experience.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>{experience.date}</Typography>
+                      <Typography variant="body2" className='timeline_description'>{experience.description}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
 
+                ))}
+              </CustomTimeline>
+            </Grid>
+
+            {/* Education */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title={"Work Education"} icon={<SchoolIcon />} children={""}  >
+                {resumeData.educations.map((education) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className='timeline_title' >{education.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>{education.date}</Typography>
+                      <Typography variant="body2" className='timeline_description'>{education.description}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+
+                ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Services */}
+      <Grid container className='section pb_45'>
+        <Grid item className='section_title mb_30'>
+          <span></span>
+          <h6 className='section_title_text'>My Services</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={3} justify={"space-around"}>
+            {resumeData.services.map((service) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <div className='service'>
+                  <Icon className='service_icon'>{service.icon}</Icon>
+                  <Typography className='service_title' variant='h6'>{service.title}</Typography>
+                  <Typography className='service_description' variant='body2'>{service.description}</Typography>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
       </Grid>
 
       {/* Skills */}
-      <Grid container className='section'>
+      <Grid container  className='section graybg pb-45 p_50'>
+        <Grid item xs={12}>
+          <Grid container justify='space-between' spacing={3}>
+            {
+              resumeData.skills.map((skill) => (
+                <Grid item xs={12} sm={6} md={3}>
+                  <Paper elevation={0} className="skill">
+                    <Typography variant='h6' className='skill_title'>
+                      {skill.title}
+                    </Typography>
+                    {
+                      skill.description.map((element) => (<Typography variant='body2' className="skill_description"><TimelineDot variant={"outlined"} className="timeline_dot" />{element}</Typography>))
+                    }
+                  </Paper>
+                </Grid>))
+            }
+          </Grid>
+        </Grid>
 
       </Grid>
 
       {/* Contact */}
       <Grid container className='section'>
-
+        <Grid item className='section_title mb_30'>
+          <span></span>
+          <h6 className='section_title_text'>About Me</h6>
+        </Grid>
       </Grid>
     </>
 
--- frontend/src/utils/resumeData.jsx
+++ frontend/src/utils/resumeData.jsx
@@ -2,6 +2,7 @@ import FacebookIcon from "@material-ui/icons/Facebook"
 import TwitterIcon from "@material-ui/icons/Twitter"
 import GithubIcon from "@material-ui/icons/GitHub"
 import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import { AssessmentOutlined, WebOutlined } from "@material-ui/icons"
 
 export default {
     name: "ABDALLAH ATGUIRI",
@@ -33,6 +34,79 @@ export default {
         }
 
     },
-    about: "🌱 I’m currently learning Javascrept, React.js, Laravel, NodeJs \n\n 🤝 I’m looking for help with my professional career \n\n 📝 I regularly write articles on atguiri.com \n\n📫 How to reach me AbdallahAtguiri @gmail.com \n\n📄 Know about my experiences atguiri.com \n\n⚡ Fun fact When I am not coding you can find me playing gym or having fun with friends."
+    about: "🌱 I’m currently learning Javascrept, React.js, Laravel, NodeJs \n\n 🤝 I’m looking for help with my professional career \n\n 📝 I regularly write articles on atguiri.com \n\n📫 How to reach me AbdallahAtguiri @gmail.com \n\n📄 Know about my experiences atguiri.com \n\n⚡ Fun fact When I am not coding you can find me playing gym or having fun with friends.",
+    experiences: [
+        {
+            title: "WorK 1",
+            date: "2021 - Present",
+            description: "Stage Développeur web Full Stack A Développeur-informatique.ma - Agadir"
+        },
+        {
+            title: "WorK 2",
+            date: "2018 - 2020",
+            description: "Stage Développeur web Full Stack A Développeur-informatique.ma - Agadir"
+        },
+        {
+            title: "WorK 3",
+            date: "Aug 2016 - Sept 2018",
+            description: "Stage Développeur web Full Stack A Développeur-informatique.ma - Agadir"
+        },
+    ],
+
+    educations: [
+        {
+            title: "education 1",
+            date: "2021 - Present",
+            description: "Stage Développeur web Full Stack A Développeur-informatique.ma - Agadir"
+        },
+        {
+            title: "education 2",
+            date: "2018 - 2020",
+            description: "Stage Développeur web Full Stack A Développeur-informatique.ma - Agadir"
+        },
+        {
+            title: "education 3",
+            date: "Aug 2016 - Sept 2018",
+            description: "Stage Développeur web Full Stack A Développeur-informatique.ma - Agadir"
+        },
+    ],
+
+    services: [
+        {
+            title: "Web Dev",
+            description: "i have a been web developer for 2 years",
+            icon: <WebOutlined />
+        },
+        {
+            title: "Web Dev",
+            description: "i have a been web developer for 2 years",
+            icon: <AssessmentOutlined/>
+        },
+        {
+            title: "Web Dev",
+            description: "i have a been web developer for 2 years",
+            icon: <WebOutlined />
+        }
+    ],
+
+    skills: [
+        {
+            title: "FRONT-END",
+            description: ["ReactJS", "JavaScript", "TypeScript", "Bootstrap", "Material UI"]
+        },
+        {
+            title: "BACK-END",
+            description: ["NodeJS", "Express", "GraphQL", "JWT"]
+        },
+        {
+            title: "DATABASES",
+            description: ["Firebase", "My SQL", "MongoDB", "Oraqel"]
+        },
+        {
+            title: "SOURCE CONTROL",
+            description: ["Git", "GitHub", "SCUM/Agile", "Doker"]
+        },
+
+    ]
 
 }
